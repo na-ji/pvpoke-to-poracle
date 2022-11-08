8f0140dd40610053d38328ec91c1c29b5fc5b357
Naji Astier <naji.astier@algoan.com>
2022-11-08 22:58:55 +0100

    feat: generate automatically pvp data

---
+++ README.md
@@ -0,0 +1,95 @@
+# PvPoke.com to Poracle
+A simple repo that convert the rankings of PvPoke.com to Poracle commands or Pokémon aliases. 
+All of those commands/aliases are updated automatically on the CI.
+
+## Pokemon aliases
+<!-- aliases-start -->
+```json
+{
+  "top30Great": [
+    618, 379, 709, 260, 411, 308, 334, 108, 560, 660, 340, 832, 799, 68, 31,
+    867, 227, 460, 365, 784, 171, 377, 748, 351, 630, 478, 302, 488, 184
+  ],
+  "top30Ultra": [
+    379, 638, 487, 709, 260, 377, 488, 208, 31, 640, 794, 799, 618, 862, 365,
+    788, 663, 560, 378, 460, 144, 149, 832, 630, 241, 186, 649, 9, 596
+  ],
+  "top30Master": [
+    249, 149, 487, 648, 383, 150, 888, 717, 644, 716, 671, 643, 483, 130, 250,
+    484, 893, 809, 645, 468, 649, 445, 143, 376, 260, 473, 794, 635, 382
+  ]
+}
+```
+<!-- aliases-end -->
+
+## Commands
+
+### Rank 1 of the top 30 Pokémon in great league ranking
+<!-- top30great-start -->
+```
+!track 618 great1
+!track 379 great1
+!track 709 great1
+!track 260 great1
+!track 411 great1
+!track 308 great1
+!track 334 great1
+!track 108 great1
+!track 560 great1
+!track 660 great1
+!track 340 great1
+!track 832 great1
+!track 799 great1
+!track 68 great1
+!track 31 great1
+!track 867 great1
+!track 227 great1
+!track 460 great1
+!track 365 great1
+!track 784 great1
+!track 171 great1
+!track 377 great1
+!track 748 great1
+!track 351 great1
+!track 630 great1
+!track 478 great1
+!track 302 great1
+!track 488 great1
+!track 184 great1
+```
+<!-- top30great-end -->
+
+### Rank 1 of the top 30 Pokémon in ultra league ranking
+<!-- top30ultra-start -->
+```
+!track 379 ultra1
+!track 638 ultra1
+!track 487 ultra1
+!track 709 ultra1
+!track 260 ultra1
+!track 377 ultra1
+!track 488 ultra1
+!track 208 ultra1
+!track 31 ultra1
+!track 640 ultra1
+!track 794 ultra1
+!track 799 ultra1
+!track 618 ultra1
+!track 862 ultra1
+!track 365 ultra1
+!track 788 ultra1
+!track 663 ultra1
+!track 560 ultra1
+!track 378 ultra1
+!track 460 ultra1
+!track 144 ultra1
+!track 149 ultra1
+!track 832 ultra1
+!track 630 ultra1
+!track 241 ultra1
+!track 186 ultra1
+!track 649 ultra1
+!track 9 ultra1
+!track 596 ultra1
+```
+<!-- top30ultra-end -->
---
+++ generator/index.ts
@@ -0,0 +1,107 @@
+import fetch from 'node-fetch';
+import path from 'path';
+import prettier from 'prettier';
+import { readFile, writeFile } from 'fs/promises';
+
+interface Pokemon {
+  dex: number;
+  speciesId: string;
+}
+
+interface Ranking {
+  speciesId: string;
+}
+
+const fetchPokemonList = async (): Promise<Pokemon[]> => {
+  console.log('Fetching game master');
+
+  const { pokemon } = (await (
+    await fetch('https://pvpoke.com/data/gamemaster.min.json', {
+      referrer: 'https://pvpoke.com/rankings/',
+      body: null,
+      method: 'GET',
+    })
+  ).json()) as { pokemon: Pokemon[] };
+
+  return pokemon;
+};
+
+const pokemonList = await fetchPokemonList();
+
+const fetchRanking = async (rankingName = 'rankings-1500'): Promise<Ranking[]> => {
+  console.log(`Fetching ${rankingName}`);
+
+  return (await (
+    await fetch(`https://pvpoke.com/data/rankings/all/overall/${rankingName}.json`, {
+      referrer: 'https://pvpoke.com/rankings/',
+      body: null,
+      method: 'GET',
+    })
+  ).json()) as Ranking[];
+};
+
+const rankingToDexList = (ranking: Ranking[], rankingSize = 30): number[] => {
+  return ranking
+    .slice(0, rankingSize * 2)
+    .map((rank) => {
+      const pokemon = pokemonList.find((pokemon) => pokemon.speciesId === rank.speciesId);
+
+      if (!pokemon) {
+        throw new Error(`Failed to find species ${rank.speciesId}`);
+      }
+
+      return pokemon.dex;
+    })
+    .filter((item, pos, self) => {
+      return self.indexOf(item) === pos;
+    })
+    .slice(0, rankingSize - 1);
+};
+
+const writeToReadme = async (textToWrite: string, tag: string): Promise<void> => {
+  console.log(`Updating readme with ${tag}`);
+  const readmePath = path.join(__dirname, '..', 'README.md');
+  const readme = await readFile(readmePath, { encoding: 'utf-8' });
+
+  const startTag = `<!-- ${tag}-start -->`;
+  const endTag = `<!-- ${tag}-end -->`;
+  const replaceRegex = new RegExp(startTag + '([^.]+)' + endTag, 'gm');
+
+  await writeFile(readmePath, readme.replace(replaceRegex, `${startTag}\n${textToWrite}\n${endTag}`));
+};
+
+const writeAliases = async (aliases: Record<string, number[]>): Promise<void> => {
+  console.log('Writing Pokémon aliases');
+
+  const pokemonAliasPath = path.join(__dirname, '..', 'pokemonAlias.json');
+
+  const aliasesOutput = prettier.format(JSON.stringify(aliases), { parser: 'json' });
+
+  await writeFile(pokemonAliasPath, aliasesOutput);
+  await writeToReadme(`\`\`\`json\n${aliasesOutput}\`\`\``, 'aliases');
+};
+
+const dexListToPoracleCommands = (dexList: number[], leagueRanking: string): string => {
+  return dexList.map((dexNumber) => `!track ${dexNumber} ${leagueRanking}`).join(`\n`);
+};
+
+const writePoracleCommands = async (poracleCommands: string, fileName: string): Promise<void> => {
+  console.log(`Writing commands ${fileName}`);
+  const poracleCommandsPath = path.join(__dirname, '..', `${fileName}.txt`);
+
+  await writeFile(poracleCommandsPath, poracleCommands);
+  await writeToReadme(`\`\`\`\n${poracleCommands}\n\`\`\``, fileName);
+};
+
+const greatLeagueRankings = await fetchRanking('rankings-1500');
+const ultraLeagueRankings = await fetchRanking('rankings-2500');
+const masterLeagueRankings = await fetchRanking('rankings-10000');
+
+await writeAliases({
+  top30Great: rankingToDexList(greatLeagueRankings),
+  top30Ultra: rankingToDexList(ultraLeagueRankings),
+  top30Master: rankingToDexList(masterLeagueRankings),
+});
+
+await writePoracleCommands(dexListToPoracleCommands(rankingToDexList(greatLeagueRankings), 'great1'), 'top30great');
+await writePoracleCommands(dexListToPoracleCommands(rankingToDexList(ultraLeagueRankings), 'ultra1'), 'top30ultra');
